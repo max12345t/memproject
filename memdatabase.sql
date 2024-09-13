67fa09b71a0c1cf8682eb2c67c29f118e4cda602
Max <maxmaxmax303@gmail.com>
2024-09-13 16:43:21 +0800

    2

--- memdatabase.sql
+++ memdatabase.sql
@@ -1,7 +1,7 @@
-//創建database
+#創建database
 create database memproject;
 use memproject;
-//創建table
+#創建table
 CREATE TABLE vocabulary_group (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(45) NULL,
@@ -26,7 +26,7 @@ CREATE TABLE vocabulary (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-//修改table格式
+#修改table格式
 ALTER TABLE vocabulary
 MODIFY days_reviewed DOUBLE  DEFAULT 1,
 MODIFY next_review_days DOUBLE DEFAULT 1,
@@ -36,14 +36,30 @@ MODIFY lasttime_difficult INT DEFAULT 0;
 Alter table vocabulary_group
 ADD CONSTRAINT unique_column UNIQUE (name);
 
-//創建第一個資料集(名稱) 編號自動增加
+#創建第一個資料集(名稱) 編號自動增加
 insert into vocabulary_group(name) values('abc');
-//匯入資料格式(資料集編號,英文,中文)
+#匯入資料格式(資料集編號,英文,中文)
 insert into vocabulary(vocabulary_group_id,name,meaning) values(1,'apple','蘋果');
-
 
 select * from vocabulary;
 select * from vocabulary_group;
-//測試歸零用
+#測試歸零用
 UPDATE `memproject`.`vocabulary` SET `days_reviewed` = '1', `next_review_days` = '1', `difficult` = '2.5', `next_review_dates` = '2024-09-10' WHERE (`id` = '1') and (`category_id` = '1');
 UPDATE `memproject`.`vocabulary` SET `days_reviewed` = '1', `next_review_days` = '1', `difficult` = '2.5', `next_review_dates` = '2024-09-10' WHERE (`id` = '2') and (`category_id` = '1');
+
+
+#以下是回饋圖表用資料
+
+create table history_record(
+test_date date ,
+vocabulary_id int,
+test_result int, 
+FOREIGN KEY (vocabulary_id) REFERENCES vocabulary(id) ,
+primary key(test_date,vocabulary_id)
+);
+#單字id p_k 外
+#測驗日期 p_k 
+#測驗結果
+
+select * from history_record;   
+
